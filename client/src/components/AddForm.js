08fceaa52fdf0bdb727dae24b65197483f5c791e
jisooyu <jisooyu@hotmail.com>
2021-06-28 08:59:39 +0900

    first deploy to heroku

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import axios from 'axios'
+
 import { addData } from '../actions/index'
-import { Button } from 'reactstrap';
+
 
 // store에 mongodb에서 불러온 데이터를 dispatch해야 함. 
 class AddForm extends Component {
     state = {
         dataFetched:false,
         dataUpdated: false,
-        location: '서초구',
+        location: '',
         error: ""
     }
 
@@ -40,25 +42,26 @@ class AddForm extends Component {
  
     render () {
         return (
-            <div >
-
-                <p>UpdateDB Status:
-                    <span > { this.state.dataUpdated ? "Data updated" : "" }</span>
-                </p>
-
-                <form className="form" onSubmit={this.onUpdateSubmit}>
-                    <label >Enter the location:{this.state.location}</label>
-                    <input onChange={ this.onLocationChange } className=" text-input " />
+            <div style={{marginTop: "1rem"}}>
+                <Form onSubmit={this.onUpdateSubmit}>
+                    <FormGroup>
+                    <Label >Enter the location:{this.state.location}</Label>
+                    
+                    <Input onChange={ this.onLocationChange } Input/>
                     <br></br>
+                    <p>UpdateDB Status:
+                        <span > { this.state.dataUpdated ? "Data updated! Click Add Button." : "No data to update!" }</span>
+                    </p>
+                   
                     {
-                        this.state.dataFetched === false ?
+                        this.state.dataFetched === false ? 
                         <Link to={'/display'}>
-                            <Button variant="primary" onClick={this.onAddClick} className="button">Add Data</Button>
+                            <Button color="primary" onClick={this.onAddClick} >Add Data</Button>{' '}
                         </Link>
                         : ""
                     }
-                       
-                </form>
+                    </FormGroup>
+                </Form>
 
                 { this.state.dataUpdated === true && this.state.dataFetched === true ?
                         <p>Save Status: New Data Saved </p>
